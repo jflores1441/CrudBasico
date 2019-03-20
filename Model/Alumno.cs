fdf262584aaf5f16d470c95cfec800a10cd8a173
Jessica Flores <jeflores@socasesores.com>
2019-03-20 17:49:34 -0600

    Crea Modelos por CodeFirst, crea Layout,  crea controlador Home, crea Vista Index, incorpora referencias a bootstrap y jquery

---
+++ Model/Alumno.cs
@@ -0,0 +1,41 @@
+namespace Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Alumno")]
+    public partial class Alumno
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Alumno()
+        {
+            Adjunto = new HashSet<Adjunto>();
+            AlumnoCurso = new HashSet<AlumnoCurso>();
+        }
+
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Apellido { get; set; }
+
+        public int Sexo { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string FechaNacimiento { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Adjunto> Adjunto { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AlumnoCurso> AlumnoCurso { get; set; }
+    }
+}
---
+++ Model/TestContext.cs
@@ -0,0 +1,38 @@
+namespace Model
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class TestContext : DbContext
+    {
+        public TestContext()
+            : base("name=TestContext")
+        {
+        }
+
+        public virtual DbSet<Adjunto> Adjunto { get; set; }
+        public virtual DbSet<Alumno> Alumno { get; set; }
+        public virtual DbSet<AlumnoCurso> AlumnoCurso { get; set; }
+        public virtual DbSet<Curso> Curso { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {          
+            modelBuilder.Entity<Alumno>()
+                .HasMany(e => e.Adjunto)
+                .WithRequired(e => e.Alumno)
+                .HasForeignKey(e => e.Alumno_id);
+
+            modelBuilder.Entity<Alumno>()
+                .HasMany(e => e.AlumnoCurso)
+                .WithRequired(e => e.Alumno)
+                .HasForeignKey(e => e.Alumno_id);           
+
+            modelBuilder.Entity<Curso>()
+                .HasMany(e => e.AlumnoCurso)
+                .WithRequired(e => e.Curso)
+                .HasForeignKey(e => e.Curso_id);
+        }
+    }
+}
